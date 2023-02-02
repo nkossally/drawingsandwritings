f71730f4f12edb0bccc14068a38f8e578a78d146
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2023-02-02 15:46:41 -0500

    install gh pages

--- src/index.js
+++ src/index.js
@@ -13,9 +13,6 @@ import {
 } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// const location = useLocation();
-// console.log("useLocationfsf", location);
-// const blarg = ()=>{
 
 root.render(
 
